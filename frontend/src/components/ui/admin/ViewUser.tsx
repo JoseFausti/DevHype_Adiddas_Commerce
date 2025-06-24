30358daa115373aa7fc13dc14ae7ed8f2e10be18
JoseFausti <joseemilianofausti@gmail.com>
2025-06-23 22:14:36 -0300

    Fixes

--- frontend/src/components/ui/admin/ViewUser.tsx
+++ frontend/src/components/ui/admin/ViewUser.tsx
@@ -1,11 +1,47 @@
-import Styles from './ViewUser.module.css'
+import { Close } from "@mui/icons-material";
+import { IUser } from "../../../types/types";
+import styles from "./ViewUser.module.css";
+import { Dispatch, SetStateAction } from "react";
+import { Modal, IconButton, Typography } from "@mui/material";
 
-const ViewUser = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface ViewUserProps {
+  user: IUser;
+  setModal: Dispatch<SetStateAction<{ view: boolean; edit: boolean }>>;
+  open: boolean;
 }
 
-export default ViewUser
+const ViewUser = ({ user, setModal, open }: ViewUserProps) => {
+  return (
+    <Modal open={open} onClose={() => setModal({ view: false, edit: false })}>
+      <div className={styles.modalBox}>
+        <header className={styles.modalHeader}>
+          <Typography component="h2" className={styles.modalTitle}>
+            {user.username}
+          </Typography>
+          <IconButton
+            onClick={() => setModal({ view: false, edit: false })}
+            className={styles.closeButton}
+            aria-label="Cerrar"
+          >
+            <Close />
+          </IconButton>
+        </header>
+
+        <div className={styles.userInfo}>
+          <strong>Nombre:</strong> {user.name}
+        </div>
+        <div className={styles.userInfo}>
+          <strong>Apellido:</strong> {user.surname}
+        </div>
+        <div className={styles.userInfo}>
+          <strong>Correo:</strong> {user.email}
+        </div>
+        <div className={styles.userInfo}>
+          <strong>Rol:</strong> {user.role}
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default ViewUser;
